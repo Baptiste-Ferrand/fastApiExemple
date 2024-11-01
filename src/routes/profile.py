7f73138f9d0c7902d58757280d601e20f5cdffea
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2024-11-01 19:12:42 +0100

    remove some usuless comments

--- src/routes/profile.py
+++ src/routes/profile.py
@@ -9,26 +9,6 @@ from src.utils.jwt_handler import verify_access_token
 
 router = APIRouter()
 
-# @router.post("/", response_model=UserResponse, status_code=201)
-# async def create_user_route(user: UserCreate, db: AsyncSession = Depends(get_db)):
-#     return await create_user(user, db)
-
-# @router.delete("/{user_uuid}", status_code=204)
-# async def delete_user_route(user_uuid: str, db: AsyncSession = Depends(get_db)):
-#     await delete_user(user_uuid, db)
-
-# @router.get("/{user_uuid}", response_model=UserResponse, status_code=200)
-# async def get_user_route(user_uuid: str, db: AsyncSession = Depends(get_db)):
-#     return await get_user(user_uuid, db)
-
-# @router.get("/", response_model=List[UserResponse], status_code=200)
-# async def get_all_users_route(db: AsyncSession = Depends(get_db)):
-#     return await get_all_users(db)
-
-# @router.put("/{user_uuid}", response_model=UserResponse, status_code=200)
-# async def update_user_route(user_uuid: str, user: UserCreate, db: AsyncSession = Depends(get_db)):
-#     return await update_user(user_uuid, user, db)
-
 @router.patch("/firstname", response_model=ProfileResponseFirstname, status_code=200)
 async def update_firstname_route(
     profile_update: ProfileUpdateFristname,
--- src/services/profile.py
+++ src/services/profile.py
@@ -7,7 +7,6 @@ from sqlalchemy.exc import NoResultFound
 from uuid import UUID
 
 
-
 async def create_profile_for_user_from_db(user_id: str, profile_data: ProfileCreate, db: AsyncSession):
     print("firstame", profile_data.firstname)
     new_profile  = Profile(
@@ -21,43 +20,6 @@ async def create_profile_for_user_from_db(user_id: str, profile_data: ProfileCre
     await db.commit()
     await db.refresh(new_profile )
     return new_profile 
-
-# async def delete_user_from_db(user_uuid: str, db: AsyncSession):
-#     result = await db.execute(select(User).where(User.uuid == UUID(user_uuid)))
-#     user = result.scalars().first()
-#     if not user:
-#         raise NoResultFound("User not found")
-    
-#     await db.delete(user)
-#     await db.commit()
-
-# async def fetch_user_from_db(user_uuid: str, db: AsyncSession):
-#     result = await db.execute(select(User).where(User.uuid == UUID(user_uuid)))
-#     user = result.scalars().first()
-#     if not user:
-#         raise NoResultFound("User not found")
-#     return user
-
-# async def fetch_all_users(db: AsyncSession):
-#     result = await db.execute(select(User))
-#     return result.scalars().all()
-
-# async def update_user_in_db(user_uuid: str, user_data: ProfileCreate, db: AsyncSession):
-#     result = await db.execute(select(User).where(User.uuid == UUID(user_uuid)))
-#     user = result.scalars().first()
-    
-#     if not user:
-#         raise NoResultFound("User not found")
-    
-#     user.firstname = user_data.firstname
-#     user.name = user_data.name
-#     user.age = user_data.age
-#     user.weight = user_data.weight
-    
-#     db.add(user)
-#     await db.commit()
-#     await db.refresh(user)
-#     return user
 
 
 async def get_profil_by_user_id(user_id: str, db: AsyncSession):
