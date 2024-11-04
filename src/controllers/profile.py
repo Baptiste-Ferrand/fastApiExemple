aea7bd4c6fbf7026b67fb140296cd9e572a6098f
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2024-11-04 16:20:53 +0100

    using the handle in and udpate the handle

--- src/controllers/profile.py
+++ src/controllers/profile.py
@@ -1,114 +1,103 @@
-from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.schemas.profile import ProfileUpdateFristname, ProfileResponseFirstname, ProfileResponseName, ProfileUpdateName, ProfileUpdateAge, ProfileResponseAge, ProfileUpdateWeight, ProfileResponseWeight,ProfileResponseHeight, ProfileUpdateHeight 
-from src.services.profile import update_firstname_profile_in_db, update_name_profile_in_db, update_age_profile_in_db, update_weight_profile_in_db, update_height_profile_in_db
-from sqlalchemy.exc import NoResultFound
-from src.validators.profile import validate_user_data, validate_uuid
-from sqlalchemy.exc import IntegrityError
+from src.schemas.profile import (
+    ProfileUpdateFristname,
+    ProfileResponseFirstname,
+    ProfileResponseName,
+    ProfileUpdateName,
+    ProfileUpdateAge,
+    ProfileResponseAge,
+    ProfileUpdateWeight,
+    ProfileResponseWeight,
+    ProfileResponseHeight,
+    ProfileUpdateHeight,
+)
+from src.services.profile import (
+    update_firstname_profile_in_db,
+    update_name_profile_in_db,
+    update_age_profile_in_db,
+    update_weight_profile_in_db,
+    update_height_profile_in_db,
+)
+from src.utils.exception_handle import handle_exception
 
 
-async def update_firstname_profile(token: dict, ProfileUpdate: ProfileUpdateFristname, db: AsyncSession):
-    user_id= token.get("sub")
+async def update_firstname_profile(
+    token: dict, ProfileUpdate: ProfileUpdateFristname, db: AsyncSession
+):
+    user_id = token.get("sub")
 
-    try: 
-        update_profile = await update_firstname_profile_in_db(user_id, ProfileUpdate.firstname, db)
+    try:
+        update_profile = await update_firstname_profile_in_db(
+            user_id, ProfileUpdate.firstname, db
+        )
         return ProfileResponseFirstname(
-            firstname= update_profile.firstname,
-            uuid= str(update_profile.uuid)
+            firstname=update_profile.firstname, uuid=str(update_profile.uuid)
         )
-            
-    except IntegrityError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Can't be the same"
-        )
+
     except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Error updating email: {str(e)}"
+        handle_exception(e, "Error when udpate firstname")
+
+
+async def update_name_profile(
+    token: dict, ProfileUpdate: ProfileUpdateName, db: AsyncSession
+):
+    user_id = token.get("sub")
+
+    try:
+        update_profile = await update_name_profile_in_db(
+            user_id, ProfileUpdate.name, db
         )
-
-async def update_name_profile(token: dict, ProfileUpdate: ProfileUpdateName, db: AsyncSession):
-    user_id= token.get("sub")
-
-    try: 
-        update_profile = await update_name_profile_in_db(user_id, ProfileUpdate.name, db)
         return ProfileResponseName(
-            name= update_profile.name,
-            uuid= str(update_profile.uuid)
+            name=update_profile.name, uuid=str(update_profile.uuid)
         )
-            
-    except IntegrityError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Can't be the same"
-        )
+
     except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Error updating email: {str(e)}"
-        )
+        handle_exception(e, "Error when udpate name")
+    
 
-async def update_age_profile(token: dict, ProfileUpdate: ProfileUpdateAge, db: AsyncSession):
-    user_id= token.get("sub")
 
-    try: 
+async def update_age_profile(
+    token: dict, ProfileUpdate: ProfileUpdateAge, db: AsyncSession
+):
+    user_id = token.get("sub")
+
+    try:
         update_profile = await update_age_profile_in_db(user_id, ProfileUpdate.age, db)
-        return ProfileResponseAge(
-            age= update_profile.age,
-            uuid= str(update_profile.uuid)
-        )
-            
-    except IntegrityError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Can't be the same"
-        )
+        return ProfileResponseAge(age=update_profile.age, uuid=str(update_profile.uuid))
+
     except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Error updating email: {str(e)}"
+        handle_exception(e, "Error when udpate age")
+
+
+async def update_weight_profile(
+    token: dict, ProfileUpdate: ProfileUpdateWeight, db: AsyncSession
+):
+    user_id = token.get("sub")
+
+    try:
+        update_profile = await update_weight_profile_in_db(
+            user_id, ProfileUpdate.weight, db
         )
-
-async def update_weight_profile(token: dict, ProfileUpdate: ProfileUpdateWeight, db: AsyncSession):
-    user_id= token.get("sub")
-
-    try: 
-        update_profile = await update_weight_profile_in_db(user_id, ProfileUpdate.weight, db)
         return ProfileResponseWeight(
-            weight= update_profile.weight,
-            uuid= str(update_profile.uuid)
+            weight=update_profile.weight, uuid=str(update_profile.uuid)
         )
-            
-    except IntegrityError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Can't be the same"
-        )
+
     except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Error updating email: {str(e)}"
+        handle_exception(e, "Error when udpate weight")
+
+
+async def update_height_profile(
+    token: dict, ProfileUpdate: ProfileUpdateHeight, db: AsyncSession
+):
+    user_id = token.get("sub")
+
+    try:
+        update_profile = await update_height_profile_in_db(
+            user_id, ProfileUpdate.height, db
         )
-
-
-async def update_height_profile(token: dict, ProfileUpdate: ProfileUpdateHeight, db: AsyncSession):
-    user_id= token.get("sub")
-
-    try: 
-        update_profile = await update_height_profile_in_db(user_id, ProfileUpdate.height, db)
         return ProfileResponseHeight(
-            height= update_profile.height,
-            uuid= str(update_profile.uuid)
+            height=update_profile.height, uuid=str(update_profile.uuid)
         )
-            
-    except IntegrityError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Can't be the same"
-        )
+
     except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Error updating email: {str(e)}"
-        )
+        handle_exception(e, "Error when udpate height")
