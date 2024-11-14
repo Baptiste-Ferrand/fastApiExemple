91e021bcdaa6945a37035dbd6d586ff67fab7ca4
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2024-11-14 17:05:19 +0100

    put back password_hashed to password

--- src/models/user.py
+++ src/models/user.py
@@ -8,7 +8,7 @@ class User(Base):
     __tablename__ = "users"
     uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     email = Column(String, unique=True, index=True, nullable=False)
-    password_hash = Column(String, nullable=False)
+    password = Column(String, nullable=False)
     role_id = Column(Integer, ForeignKey('roles.id'))
 
     role = relationship("Role", back_populates="users")
