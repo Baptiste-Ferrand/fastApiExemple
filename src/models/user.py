7334f69417712aa4753326b62f1fc68ca0bab633
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2024-11-14 16:44:10 +0100

    update the model

--- src/models/user.py
+++ src/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 import uuid
@@ -7,6 +7,9 @@ from src.models import Base
 class User(Base):
     __tablename__ = "users"
     uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    role_id = Column(Integer, ForeignKey('roles.id'))
+
+    role = relationship("Role", back_populates="users")
     profile = relationship("Profile", back_populates="user", uselist=False, cascade="all, delete")
