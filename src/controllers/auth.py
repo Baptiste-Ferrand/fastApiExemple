b7698634e559d9f9290fd97a2e00b72ae3b1808f
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2024-11-20 12:12:30 +0100

    update auth for put the role in the token when loging

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -5,15 +5,15 @@ from src.utils.jwt_handler import create_access_token
 from sqlalchemy.ext.asyncio import AsyncSession
 
 async def login_user(user_credentials: LoginUser, db: AsyncSession) -> TokenResponse:
-    user = await authenticate_user(user_credentials.email, db)
+    user, user_role = await authenticate_user(user_credentials.email, db)
 
     handle_auth_veryfication(user, user_credentials.password)
     
-    token_data = {"sub": str(user.uuid), "email": user.email}
+    token_data = {"sub": str(user.uuid), "email": user.email, "role": user_role.name}
     access_token = create_access_token(data=token_data)
     
     return TokenResponse(
         access_token=access_token,
         token_type="bearer",
-        user=LoginResponse(uuid=str(user.uuid), email=user.email)
+        user=LoginResponse(uuid=str(user.uuid), email=user.email, role= user_role.name)
     )
--- src/services/auth.py
+++ src/services/auth.py
@@ -1,8 +1,17 @@
 from src.models.user import User
+from src.models.role import Role
+from src.schemas.auth import LoginResponse
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 async def authenticate_user(email: str, db: AsyncSession):
     result = await db.execute(select(User).where(User.email == email))
     user = result.scalars().first()
-    return user  
+    
+    role_result = await db.execute(
+        select(Role).where(Role.id == user.role_id)
+    )
+    user_role = role_result.scalars().first()
+
+    print(user_role.name, "mon user loging")
+    return user, user_role
